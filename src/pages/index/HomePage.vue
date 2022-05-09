b48233a0eb7f249d31d329c9c8642af2ee6a9f1e
AreaLiang <137739760@qq.com>
2022-05-09 18:03:27 +0800

    5-9 下午日常更新

---
+++ src/pages/index/HomePage.vue
@@ -0,0 +1,152 @@
+<template>
+	<view class="home-page">
+		<view class="box-bg">
+			<uni-search-bar class="uni-mt-10" radius="5" placeholder="自动显示隐藏" clearButton="auto" cancelButton="none"
+				@focus="focus" />
+		</view>
+		<uni-row class="scroll-box">
+			<uni-col :span="23">
+				<scroll-view class="scroll-view_H" scroll-x="true" style="white-space: nowrap;" @scroll="scroll"
+					scroll-left="120">
+					<view class="top-title-box">
+						<view class="top-title">
+							<span>关注</span>
+							<span>推荐</span>
+							<span>肇庆</span>
+							<span>视频</span>
+							<span>小说</span>
+							<span>小视频</span>
+							<span>抗疫</span>
+							<span>问答</span>
+						</view>
+					</view>
+				</scroll-view>
+			</uni-col>
+			<uni-col :span="2"
+				style="position: absolute;right: 0; background-color: rgb(245 245 245 / 85%);text-align:center;top:50%;transform: translateY(-50%);">
+				<view class="more-box">
+					<uni-icons type="bars" size="1.5em"></uni-icons>
+				</view>
+			</uni-col>
+		</uni-row>
+		<view class="content">
+			<view class="item">
+				<h3 class="title">疫情防不住，经济社会发展就无从谈起</h3>
+				<view class="article-info">
+					<span>光明网</span>
+					<span>6941 评论</span>
+					<span>5小时前</span>
+				</view>
+			</view>
+			<view class="item">
+				<h3 class="title">坐月子无聊破译顶级密码？真相来了</h3>
+				<view class="article-info">
+					<span>北青网</span>
+					<span>6236 评论</span>
+					<span>11小时前</span>
+				</view>
+			</view>
+			<view class="item">
+				<h3 class="title">“无论我走到哪，永远是黄土地的儿子”</h3>
+				<view class="article-info">
+					<span>新华社</span>
+					<span>9237 评论</span>
+					<span>1周前</span>
+				</view>
+			</view>
+			<view class="item">
+				<h3 class="title">2012年上海富豪包养女大学生，7年生3娃花2000万，却没一个是他的</h3>
+				<view class="article-img">
+					<img src="/static/c1.png" alt="">
+					<img src="/static/c2.png" alt="">
+					<img src="/static/c3.png" alt="">
+				</view>
+				<view class="article-info">
+					<span>新华社</span>
+					<span>9237 评论</span>
+					<span>1周前</span>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import {
+		mapState
+	} from 'vuex'
+	export default {
+		name: 'HomePage',
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+			//搜索框聚焦事件
+			focus() {
+
+			},
+			scroll: function(e) {
+				console.log(e)
+
+			},
+		},
+		computed: {
+			...mapState(["userData"])
+		},
+		components: {
+
+		},
+		onReady() {
+			let token = localStorage.getItem('token');
+		}
+	}
+</script>
+
+<style lang="less">
+	.box-bg {
+		background: #d04542;
+	}
+
+	.scroll-box {
+		border-bottom: 1px solid #cbcaca;
+	}
+
+	.top-title span {
+		margin: 5px 10px;
+		display: inline-block;
+	}
+
+	scroll-view ::-webkit-scrollbar {
+		display: none;
+		width: 0;
+		height: 0;
+		background-color: transparent;
+	}
+	.content{
+		.item{
+			border-bottom:1px solid #d9d9d9;
+			padding: 10px;
+			color: #343434;
+		}
+		.title{
+			width: 100%;
+			overflow: hidden;
+			white-space:nowrap;
+			text-overflow:ellipsis;
+		}
+		.article-img img{
+			width: 80px;
+			height: 80px;
+			margin: 3px;
+		}
+		.article-info{
+			margin-top: 3px;
+		}
+		.article-info span{
+			margin-right: 5px;
+			color:#919191;
+		}
+	}
+</style>
---
+++ src/pages/index/User.vue
@@ -0,0 +1,306 @@
+<template>
+	<view class="userPage">
+		<!-- <view class="not-login-box login-status">
+			<img src="/static/login.png" alt="" @click="login()">
+		</view> -->
+		<uni-row class="logged-box login-status">
+			<uni-col :span="6">
+				<view class="portrait-box">
+					<img src="/static/user/touxian.jpg" alt="" class="portrait">
+				</view>
+			</uni-col>
+			<uni-col :span="18">
+				<view class="userInfo-box">
+					<view class="user-name">
+						<view class="">
+							AreaLiang
+							<uni-icons type="right" size="15"></uni-icons>
+						</view>
+						<view class="apply-certification">
+							申请认证
+						</view>
+					</view>
+					<view class="notice-box">
+						<view class="toutiao notice-item">
+							<span class="title">头条</span>
+							<span class="num">2</span>
+						</view>
+						<view class="guanzhu notice-item">
+							<span class="title">关注</span>
+							<span class="num">0</span>
+						</view>
+						<view class="fenshi notice-item">
+							<span class="title">粉丝</span>
+							<span class="num">20470</span>
+						</view>
+					</view>
+				</view>
+			</uni-col>
+		</uni-row>
+		
+		<uni-card title="常用功能" margin="10px 0 10px 0" :border="false">
+			<uni-grid :column="4" :highlight="true" @change="change" :showBorder="false">
+				<uni-grid-item v-for="(item, index) in commonFunctions" :index="index" :key="index">
+					<view class="grid-item-box" style="background-color: #fff;">
+						<uni-icons custom-prefix="iconfont" :type="item.icon" :size="25" color="#777" />
+						<text class="text">{{item.name}}</text>
+					</view>
+				</uni-grid-item>
+			</uni-grid>
+		</uni-card>
+
+		<uni-card title="更多功能" margin="10px 0 10px 0" :border="false">
+			<uni-grid :column="4" :highlight="true" @change="change" :showBorder="false">
+				<uni-grid-item v-for="(item, index) in moreFunctions" :index="index" :key="index">
+					<view class="grid-item-box" style="background-color: #fff;">
+						<uni-icons custom-prefix="iconfont" :type="item.icon" :size="25" color="#777" />
+						<text class="text">{{item.name}}</text>
+					</view>
+				</uni-grid-item>
+			</uni-grid>
+		</uni-card>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'User',
+		data() {
+			return {
+				commonFunctions: [{
+						name: "关注",
+						icon: "icon-guanzhu",
+						url: "",
+					},
+					{
+						name: "消息通知",
+						icon: "icon-xiaoxitongzhi",
+						url: "",
+					},
+					{
+						name: "收藏",
+						icon: "icon-shoucang",
+						url: "",
+					},
+					{
+						name: "浏览历史",
+						icon: "icon-liulanlishi",
+						url: "",
+					},
+					{
+						name: "钱包",
+						icon: "icon-qianbao",
+						url: "",
+					},
+					{
+						name: "用户反馈",
+						icon: "icon-feedback",
+						url: "",
+					},
+					{
+						name: "免流量服务",
+						icon: "icon-wode-faxian-mianliuliang",
+						url: "",
+					},
+					{
+						name: "系统设置",
+						icon: "icon-xitong",
+						url: "",
+					}
+				],
+				moreFunctions: [{
+						name: "订单",
+						icon: "icon-dingdan",
+						url: "",
+					},
+					{
+						name: "夜间模式",
+						icon: "icon-yejianmoshi",
+						url: "",
+					},
+					{
+						name: "评论",
+						icon: "icon-pinglun",
+						url: "",
+					},
+					{
+						name: "点赞",
+						icon: "icon-dianzan",
+						url: "",
+					},
+					{
+						name: "扫一扫",
+						icon: "icon-saoyisao",
+						url: "",
+					},
+					{
+						name: "购物车",
+						icon: "icon-gouwuche",
+						url: "",
+					}
+				]
+			}
+		},
+		methods: {
+			login() {
+				uni.navigateTo({
+					url: '/src/pages/Login'
+				})
+			}
+		},
+		components: {
+
+		}
+	}
+</script>
+
+<style scoped lang="less">
+	@import "@/static/iconfont.css";
+
+	.userPage {
+		overflow: auto;
+		height: 750px;
+	}
+
+	.logged-box {
+		padding-left: 15px;
+		display: flex;
+		align-items: center;
+		color: #2c2c2c;
+		
+		.portrait-box {
+			width: 80px;
+			height: 80px;
+			border-radius: 50%;
+			border: 1px solid #e1dfdf;
+		}
+
+		.portrait {
+			width: 100%;
+			height: 100%;
+			border-radius: 50%;
+		}
+
+		.user-name {
+			font-size: 5vw;
+			font-weight: 600;
+			display: flex;
+			justify-content: space-between;
+		}
+		
+		.apply-certification{
+			background: #fdeaea;
+			border-radius: 10px 0 0 10px;
+			width: 75px;
+			height: 25px;
+			font-size: 14px;
+			text-align: center;
+			line-height: 25px;
+			color: #fb6161;
+			font-weight: 500;
+		}
+	}
+
+	.notice-box {
+		display: flex;
+		flex-direction: row;
+		margin-top: 3px;
+
+		.notice-item {
+			margin-right: 5px;
+
+			span {
+				margin-right: 2px;
+			}
+		}
+	}
+
+	.login-status {
+		background-color: white;
+		height: 120px;
+	}
+
+
+	.not-login-box {
+		text-align: center;
+	}
+
+	.image {
+		width: 25px;
+		height: 25px;
+	}
+
+	.text {
+		font-size: 14px;
+		margin-top: 5px;
+	}
+
+	.example-body {
+		/* #ifndef APP-NVUE */
+		// display: block;
+		/* #endif */
+	}
+
+	.grid-dynamic-box {
+		margin-bottom: 15px;
+	}
+
+	.grid-item-box {
+		flex: 1;
+		// position: relative;
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding: 15px 0;
+	}
+
+	.grid-item-box-row {
+		flex: 1;
+		// position: relative;
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		padding: 15px 0;
+	}
+
+	.grid-dot {
+		position: absolute;
+		top: 5px;
+		right: 15px;
+	}
+
+	.swiper {
+		height: 420px;
+	}
+
+	/* #ifdef H5 */
+	@media screen and (min-width: 450px) {
+		.logged-box{
+			.user-name {
+				font-size: 20px;
+			}
+			.notice-box{
+				font-size: 18px;
+			}
+		}
+	}
+	@media screen and (min-width: 768px) and (max-width: 1425px) {
+		.swiper {
+			height: 630px;
+		}
+	}
+
+	@media screen and (min-width: 1425px) {
+		.swiper {
+			height: 830px;
+		}
+	}
+
+	/* #endif */
+</style>
